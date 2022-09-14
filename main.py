45ae403067580d8ff98d5ba94412676b967b3ece
Mark172004 <74767453+Maku-gif@users.noreply.github.com>
2022-09-14 07:27:48 +0000

    Een eenvoudige tegenstander gemaakt

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from bke import EvaluationAgent, start, can_win
+import random
+from bke import EvaluationAgent, start
 
-class MijnS(EvaluationAgent):
+
+class MyRandomAgent(EvaluationAgent):
   def evaluate(self, board, my_symbol, opponent_symbol):
     return random.randint(1,500)
 
 my_random_agent = MyRandomAgent()
-start(player_o=my_random_agent, player_x = my_random_agent)
+start(player_o = my_random_agent)
+  
+      
