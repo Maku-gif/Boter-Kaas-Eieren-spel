1b604928a5444f31ba876e5b0b158ca08cb2ce8c
Mark172004 <74767453+Maku-gif@users.noreply.github.com>
2022-09-28 07:28:22 +0000

    Agent trainen optie afgemaakt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def menu():
-  print("*** WELKOM BIJ HET SPELLETJE Boter-Kaas-Eieren***")
+  print("*** WELKOM BIJ HET SPELLETJE Boter-Kaas-Eieren ***")
   print("--------------------------------------------------------")
   print("Om tegen een domme tegenstander te spelen, toets [1]")
   print("Om tegen een slimme tegenstander te spelen, toets [2]")
@@ -36,6 +36,20 @@ while optie != 6:
       start()
 
     if optie == 4:
+      from bke import MLAgent, is_winner, opponent, train, save
+      class MyAgent(MLAgent):
+        def evaluate(self, board):
+          if is_winner(board, self.symbol):
+              reward = 1
+          elif is_winner(board, opponent[self.symbol]):
+              reward = -1
+          else:
+              reward = 0
+          return reward
+      my_agent = MyAgent()
+      train(my_agent, 3000)
+      save(my_agent, 'MyAgent_3000')
+
       from bke import MLAgent, is_winner, opponent, train, load, start
       class MyAgent(MLAgent):
         def evaluate(self, board):
